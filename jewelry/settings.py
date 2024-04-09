9ffcbc74ac1ab6ed694e8ec36b911cacbe9ad6a0
Sarah <grozavsarah@gmail.com>
2024-04-09 14:11:27 +0000

    added basic checkout functionality

--- jewelry/settings.py
+++ jewelry/settings.py
@@ -171,3 +171,9 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 # Default primary key field type
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Stripe
+
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')

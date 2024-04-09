a8318b25b3ae724ee6c21cc9098a41bcebb9b914
Sarah <grozavsarah@gmail.com>
2024-04-09 16:25:37 +0000

    added checkout form data cach

--- usercheckout/urls.py
+++ usercheckout/urls.py
@@ -7,5 +7,6 @@ name = 'usercheckout'
 urlpatterns = [
     path('', views.usercheckout, name='usercheckout')
     path('usercheckout_success/<order_number>', views.usercheckout_success, name='usercheckout_success'),
+    path('cache_data/', views.ucache_data, name='cache_data'),
     path('wh/', webhook, name='webhook'),
 ]

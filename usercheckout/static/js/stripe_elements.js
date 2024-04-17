838f84e830e598674a591df05b42dcefa7ac4ed8
Sarah <grozavsarah@gmail.com>
2024-04-17 17:38:35 +0000

    Updated webhook handler to handle profiles

--- usercheckout/static/js/stripe_elements.js
+++ usercheckout/static/js/stripe_elements.js
@@ -2,9 +2,6 @@
 /*
     Core logic/payment flow for this comes from here:
     https://stripe.com/docs/payments/accept-a-payment
-
-    CSS from here: 
-    https://stripe.com/docs/stripe-js
 */
 
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
@@ -14,7 +11,6 @@ var elements = stripe.elements();
 var style = {
     base: {
         color: '#000',
-        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
         fontSmoothing: 'antialiased',
         fontSize: '16px',
         '::placeholder': {

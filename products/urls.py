797fe879a5da6ac7a251feac8d0edf930c67d830
Sarah <grozavsarah@gmail.com>
2024-04-19 06:40:09 +0000

    Deleting products

--- products/urls.py
+++ products/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('<int:product_id>/', views.detail_product, name='detail_product'),
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
-
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
 ]

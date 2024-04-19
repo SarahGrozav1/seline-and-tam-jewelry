6a2bab9ad5da3202e5c65782bd5861fdae8f8be9
Sarah <grozavsarah@gmail.com>
2024-04-19 05:38:07 +0000

    Editing products

--- products/urls.py
+++ products/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.all_products, name='products'),
     path('<int:product_id>/', views.detail_product, name='detail_product'),
     path('add/', views.add_product, name='add_product'),
-    
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+
 ]

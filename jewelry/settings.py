da11712c26b9e1c2cdcb09b00538e88ff0b321fb
Sarah <grozavsarah@gmail.com>
2024-04-22 15:51:56 +0000

    Deleted one secret key

--- jewelry/settings.py
+++ jewelry/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fgo#4se)ak74_5e2@-jvrn3737jhwp@(i@ay5d_wb-7+47roqu'
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!

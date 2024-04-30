709daf73d4a0c1ae523e24a6f2d77b3a23744b86
Sarah <grozavsarah@gmail.com>
2024-04-30 20:24:41 +0000

    Added contact form

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,6 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.index, name='home'),
-    
+    path('contact_form', views.contact_form, name='contact_form' )
     
 ]
--- home/views.py
+++ home/views.py
@@ -1,16 +1,31 @@
 
 from django.shortcuts import render
-
-
+from .models import Contact
+from django.contrib import messages
 
 # Create your views here.
 
 def index(request):
     """ A view to return the index page """
 
-    # get users review 
-
     return render(request, 'home/index.html')
+
+
+def contact_form(request):
+    """ A view for contact form """
+    
+    if request.method == "POST":
+        contact = Contact()
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        contact.name = name
+        contact.email = email
+        contact.subject = subject
+        contact.save()  
+        messages.success(request, 'Thank you for contacting us')
+    return render(request, 'home/contact.html')
+
     
     
 

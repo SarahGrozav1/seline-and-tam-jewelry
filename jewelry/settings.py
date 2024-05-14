9aa357f5a35c3e9bd8e69e4c226828d556c0ba1c
Sarah <grozavsarah@gmail.com>
2024-05-14 16:45:07 +0000

    Tryingto fix env error

--- jewelry/settings.py
+++ jewelry/settings.py
@@ -16,8 +16,9 @@ from pathlib import Path
 
 if os.path.exists("env.py"):
     import env
-    # Build paths inside the project like this: BASE_DIR / 'subdir'.
-    BASE_DIR = Path(__file__).resolve().parent.parent
+    
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
